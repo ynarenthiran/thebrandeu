dd9a5a2fff398f63b06485dc9233e7b4b6df88da
Narenthiran Yoganandhan <narenthiran4019@gmail.com>
2024-12-26 19:19:10 +0530

    CHanges

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Active Menu on Scrolling
 document.addEventListener("DOMContentLoaded", () => {
   const sections = document.querySelectorAll("section");
   const menuItems = document.querySelectorAll(".menu");
@@ -25,48 +26,10 @@ document.addEventListener("DOMContentLoaded", () => {
   setActiveMenu();
 });
 
-new Typed("#our-story", {
-  strings: ["Our Story"],
-  typeSpeed: 100,
-  backSpeed: 100,
-  loop: true,
-});
-new Typed("#our-services", {
-  strings: ["What We Do"],
-  typeSpeed: 100,
-  backSpeed: 100,
-  loop: true,
-});
-new Typed("#our-clients", {
-  strings: ["Our Esteemed Clients"],
-  typeSpeed: 100,
-  backSpeed: 100,
-  loop: true,
-});
-new Typed("#our-partners", {
-  strings: ["Who We Trusts"],
-  typeSpeed: 100,
-  backSpeed: 100,
-  loop: true,
-});
+// Typewriter
 new Typed("#subscribe-text", {
   strings: ["Start Elevating Your Brand Today."],
   typeSpeed: 100,
   backSpeed: 100,
   loop: true,
 });
-
-// new Splide("#image-carousel", {
-//   autoplay: "pause",
-//   intersection: {
-//     inView: {
-//       autoplay: true,
-//     },
-//   },
-//   perPage: 3,
-//   breakpoints: {
-//     640: {
-//       perPage: 1,
-//     },
-//   },
-// });
